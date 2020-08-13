02515f76f0df4c8d0b6521f3bc7d325d2bec9d29
angelfeli <angelfelipe1398@gmail.com>
2020-08-12 22:31:02 -0500

    template basico

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.components.css']
+})
+export class HomeComponent{
+
+    constructor(){}
+}
